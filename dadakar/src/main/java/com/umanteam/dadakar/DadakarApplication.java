23f55a2fc80748ff763be29e921c8bcb44991fa8
FranckDemeyer <franck.demeyer@free.fr>
2017-08-21 09:30:40 +0200

    spring boot back-end application skeleton

---
+++ dadakar/src/main/java/com/umanteam/dadakar/DadakarApplication.java
@@ -0,0 +1,12 @@
+package com.umanteam.dadakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadakarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadakarApplication.class, args);
+	}
+}
