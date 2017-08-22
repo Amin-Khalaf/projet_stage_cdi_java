a432bd7e2ca671a2f85c6b3282d7ad1e035ed31e
arhgi128 <arhgi128@gmail.com>
2017-08-22 11:11:04 +0200

    image service done, not tested

---
+++ dadakar-back-img/src/main/java/com/umanteam/dadakarimg/back/webservice/IImageStorageWebService.java
@@ -0,0 +1,15 @@
+package com.umanteam.dadakarimg.back.webservice;
+
+import java.util.List;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IImageStorageWebService {
+
+	public void storeImage(MultipartFile file);
+	public ResponseEntity<InputStreamResource> getImageAsResource(String id);
+	public ResponseEntity<InputStreamResource> getImageByFileName(String filename);
+	public ResponseEntity<List<InputStreamResource>> getAllImages();
+}
