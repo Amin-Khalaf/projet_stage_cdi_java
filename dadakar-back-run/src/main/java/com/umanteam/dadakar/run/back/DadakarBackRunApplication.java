df8ad1a4da076227b01840f16ba99b72426038c4
arhgi128 <arhgi128@gmail.com>
2017-08-22 15:33:13 +0200

    init run project

---
+++ dadakar-back-run/src/main/java/com/umanteam/dadakar/run/back/DadakarBackRunApplication.java
@@ -0,0 +1,12 @@
+package com.umanteam.dadakar.run.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadakarBackRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadakarBackRunApplication.class, args);
+	}
+}
---
+++ dadakar-back-run/src/test/java/com/umanteam/dadakar/run/back/DadakarBackRunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.umanteam.dadakar.run.back;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DadakarBackRunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
