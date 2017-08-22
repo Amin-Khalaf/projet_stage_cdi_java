39faf8e8c3253f2160a06e883e296b1d623d15dd
arhgi128 <arhgi128@gmail.com>
2017-08-22 09:39:54 +0200

    created project for image service

---
+++ dadakar-back-img/src/main/java/com/umanteam/dadakarimg/back/DadakarBackImgApplication.java
@@ -0,0 +1,12 @@
+package com.umanteam.dadakarimg.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadakarBackImgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadakarBackImgApplication.class, args);
+	}
+}
