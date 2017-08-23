5c38eb6936aa614fe0636aeaaefc38940ff87f99
FranckDemeyer <franck.demeyer@free.fr>
2017-08-23 11:23:21 +0200

    adding Toll.java and TollDTO.java in order to develop Run bundle

---
+++ dadakar-back-run/src/main/java/com/umanteam/dadakar/run/back/dto/TollDTO.java
@@ -0,0 +1,51 @@
+package com.umanteam.dadakar.run.back.dto;
+
+import java.io.Serializable;
+
+public class TollDTO implements Serializable {
+
+	/* Variables */
+	
+	private static final long serialVersionUID = 1114260076943532254L;
+	private String tollId;
+	private String name;
+	private double price;
+	
+	public TollDTO() {
+	}
+
+	public TollDTO(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getTollId() {
+		return tollId;
+	}
+
+	public void setTollId(String tollId) {
+		this.tollId = tollId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "TollDTO [tollId=" + tollId + ", name=" + name + ", price=" + price + "]";
+	}
+	
+}
