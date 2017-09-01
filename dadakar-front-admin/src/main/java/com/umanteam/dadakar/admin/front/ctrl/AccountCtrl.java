6d990e57eda6d35a606bcc4ef8a2b8e75148b775
arhgi128 <arhgi128@gmail.com>
2017-09-01 12:45:25 +0200

    admin list and create working

--- dadakar-front-admin/src/main/java/com/umanteam/dadakar/admin/front/ctrl/AccountCtrl.java
+++ dadakar-front-admin/src/main/java/com/umanteam/dadakar/admin/front/ctrl/AccountCtrl.java
@@ -3,8 +3,11 @@ package com.umanteam.dadakar.admin.front.ctrl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.umanteam.dadakar.admin.front.dto.Account;
 import com.umanteam.dadakar.admin.front.service.interfaces.IAccountService;
 
 @Controller
@@ -12,11 +15,18 @@ import com.umanteam.dadakar.admin.front.service.interfaces.IAccountService;
 public class AccountCtrl {
 
 	@Autowired
-	IAccountService accountService;
+	private IAccountService accountService;
 	
-	@RequestMapping(value="/index")
+	@RequestMapping(value={"/", "index"})
 	public String index(Model model){
 		model.addAttribute("adminAccounts", accountService.findAdminsAndSuperUsers());
 		return "admin/index";
+	}
+	
+	@RequestMapping(value="create", method = RequestMethod.POST)
+	public String create(@ModelAttribute("accountForm") Account account){
+		System.out.println(account);
+		account = accountService.add(account);
+		return "redirect:/admin/index";
 	}
 }
