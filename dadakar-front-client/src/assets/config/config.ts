2cdac8534f8aa6302bb2c566863fc12d9f065499
FranckDemeyer <franck.demeyer@free.fr>
2017-09-19 12:18:21 +0200

    update config to localhost

--- dadakar-front-client/src/assets/config/config.ts
+++ dadakar-front-client/src/assets/config/config.ts
@@ -1,7 +1,7 @@
 export default {
-    backImgServerAddress:"http://192.168.0.76:8180",
-    backLoginServerAddress:"http://192.168.0.76:8080",
-    backMsgServerAddress:"http://192.168.0.76:8580",
-    backRunServerAddress:"http://192.168.0.76:8280",
-    backTransactionServerAddress:"http://192.168.0.76:8380"
+    backImgServerAddress:"http://localhost:8180",
+    backLoginServerAddress:"http://localhost:8080",
+    backMsgServerAddress:"http://localhost:8580",
+    backRunServerAddress:"http://localhost:8280",
+    backTransactionServerAddress:"http://localhost:8380"
 }
