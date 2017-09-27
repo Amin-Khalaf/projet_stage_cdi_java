d428d37955428427f03b8b7baf55f79b4f203585
FranckDemeyer <franck.demeyer@free.fr>
2017-09-27 16:13:37 +0200

    adding home.ts

--- dadakar-front-client/src/pages/home/home.ts
+++ dadakar-front-client/src/pages/home/home.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { LocalDate } from 'js-joda';
-import { MenuController } from 'ionic-angular';
+import { MenuController, NavController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 
 import { AuthProvider } from '../../providers/auth';
+
+import { Search } from '../../models/search.model';
+
+import { SearchResultPage } from '../../pages/search-result/search-result';
+
+import { RunService } from '../../services/run.service';
 
 @Component({
   selector: 'page-home',
@@ -15,8 +21,9 @@ export class HomePage {
     formIsValid: boolean = false;
     today: string;
     maxSearch: string;
+    searchValues: Search = null;
 
-    constructor(private authProvider: AuthProvider, private menu: MenuController) {
+    constructor(private authProvider: AuthProvider, private menu: MenuController, private navCtrl: NavController, private runService: RunService) {
         this.today = LocalDate.now().toString();
         this.maxSearch = LocalDate.now().plusDays(60).toString();
         this.authProvider.authUser.subscribe(jwt => {
@@ -41,7 +48,15 @@ export class HomePage {
     }
 
     search(values: any, form: NgForm) {
-        console.log(values);
+        this.searchValues = {
+            startTown: 'Paris',
+            startDistrict: '10 ème',
+            endTown: 'Lille',
+            endDistrict: 'Centre',
+            startDate: LocalDate.of(2017, 9, 22)
+        }
+        this.runService.setSearch(this.searchValues);
+        this.navCtrl.push(SearchResultPage);
     }
 
     validate(values: any) {
